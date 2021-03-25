9ecdd5d36ee79387f8c6fba1e6d64175f006ab63
J3eff <jeffersonvideo125@gmail.com>
2021-03-25 19:42:15 -0300

    Adicionado classe Location

---
+++ LocationCar/Model/Location.cs
@@ -0,0 +1,28 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+
+
+namespace LocationCar.Model
+{
+    class Location
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<ModelCar> Car { get; set; } = new List<ModelCar>();
+
+        public Location()
+        {
+        }
+
+        public Location(int id, DateTime date, double price, Cliente cliente)
+        {
+            Id = id;
+            Date = date;
+            Price = price;
+            Cliente = cliente;           
+        }
+    }
+}
